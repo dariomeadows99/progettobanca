6cbc46a7c607d8fc51d82f6351bb398ab4c8db2e
dariomeadows <@dariomeadows99>
2025-06-04 11:44:10 +0200

    test

--- conto_bancario.py
+++ conto_bancario.py
@@ -21,7 +21,7 @@ class ContoBancario:
             raise ValueError("L'importo non può essere negativo!")
 
         if self.saldo >= importo:
-            self.saldo -= importo
+            self.saldo -= importo -1
         else:
             raise ValueError("Non hai abbastanza soldi!")
 
